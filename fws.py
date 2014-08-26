1b73105322492e18ff42ee7ba8714b59b22640e4
Gino <ginozhao2010@gmail.com>
2014-08-26 11:21:38 +0800

    add daemon and logging fuction

--- fws.py
+++ fws.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # python ./fws.pyc >/dev/null 2>&1 &
-# netstat -lptun | grep 8000
+# netstat -lptun | grep 61980
 import BaseHTTPServer
 import ConfigParser
+from SocketServer import ThreadingMixIn
+import logging
 import threading
 import time
-
 import requests
-
+import sys
+from daemon import Daemon
 
 __author__ = 'gino'
 
@@ -17,17 +20,18 @@ LOGIN_PARAMS = {}
 MAIN_URL = ''
 MAIN_SEC = 0.5
 ACL = ()
+REPEAT = True
 # For testing
 DATA = ''
 
 
-class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    # def do_HEAD(self):
-    # self.send_response(200)
-    #     self.send_header("Content-type", "text/html;charset=GBK")
-    #     self.send_header("Server", "ForwardingServer/1.0")
-    #     self.end_headers()
+# Multithreading
+class ThreadedHTTPServer(ThreadingMixIn, BaseHTTPServer.HTTPServer):
+    pass
 
+
+# Handler
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
         """Respond to a GET request."""
         global DATA, ACL
@@ -37,16 +41,22 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.send_header("Transfer-Encoding", "chunked")
             self.end_headers()
             self.wfile.write(DATA.encode('GBK'))
-            # else:
-            #     self.send_response(404)
+        else:
+            # self.send_response(404)
+            logging.basicConfig(filename='deny.log', format='%(asctime)s - %(levelname)s - %(message)s')
+            logging.warning('%s is denied' % self.client_address[0])
+
+        message = threading.currentThread().getName()
+        print(message)
 
 
-def get_data_from_MAIN():
-    global MAIN_URL, LOGIN_PARAMS, MAIN_SEC, DATA
+def get_data_from_main():
+    global MAIN_URL, LOGIN_PARAMS, MAIN_SEC, DATA, REPEAT
     req = requests.get(MAIN_URL, params=LOGIN_PARAMS)
     DATA = req.text
-    # print(time.asctime())
-    threading.Timer(MAIN_SEC, get_data_from_MAIN).start()
+    if REPEAT:
+        # print(time.asctime())
+        threading.Timer(MAIN_SEC, get_data_from_main).start()
 
 
 # initial server config parameter
@@ -59,30 +69,49 @@ def init_config():
     HOST = config.get('Server', 'HOST')
     PORT = int(config.get('Server', 'PORT'))
 
-    #MAIN config
+    # MAIN config
     MAIN_URL = config.get('MAIN', 'URL')
     LOGIN_PARAMS['username'] = config.get('MAIN', 'USERNAME')
     LOGIN_PARAMS['password'] = config.get('MAIN', 'PASSWORD')
     MAIN_SEC = float(config.get('MAIN', 'SEC'))
 
-    #ACL config
+    # ACL config
     ACL = tuple(config.get('ACL', 'WLIST').split(','))
 
 
-if __name__ == '__main__':
-    # initial server
-    init_config()
-    #This function will start a new thread via Timer module
-    get_data_from_MAIN()
+class MyDaemon(Daemon):
+    def run(self):
+        # initial server
+        init_config()
+        # This function will start a new thread via Timer module
+        get_data_from_main()
 
-    #Start a http server
-    MyHandler.server_version = 'Light Forwarding Server/1.0'
-    MyHandler.sys_version = ''
-    httpd = BaseHTTPServer.HTTPServer((HOST, PORT), MyHandler)
-    print time.asctime(), "Server Starts - %s:%s" % (HOST, PORT)
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    print time.asctime(), "Server Stops - %s:%s" % (HOST, PORT)
+        # Start a http server
+        MyHandler.server_version = 'Light Forwarding Server/1.0'
+        MyHandler.sys_version = ''
+        httpd = ThreadedHTTPServer((HOST, PORT), MyHandler)
+        print time.asctime(), "Server Starts - %s:%s" % (HOST, PORT)
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            pass
+        httpd.server_close()
+        print time.asctime(), "Server Stops - %s:%s" % (HOST, PORT)
+
+
+if __name__ == '__main__':
+    daemon = MyDaemon('/tmp/forwardserver.pid')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
