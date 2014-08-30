f3073d4edddbc49fbb44fa668e2d9738cb3a6f7a
Gino <ginozhao2010@gmail.com>
2014-08-30 20:38:46 +0800

    optimize logging and add absolute thread for request

--- fws.py
+++ fws.py
@@ -6,10 +6,12 @@ import BaseHTTPServer
 import ConfigParser
 from SocketServer import ThreadingMixIn
 import logging
+import logging.handlers
 import threading
 import time
+
 import requests
-from daemon import runner
+
 
 __author__ = 'gino'
 
@@ -39,32 +41,33 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.send_header("Content-Type", "text/html;charset=GBK")
             self.send_header("Transfer-Encoding", "chunked")
             self.end_headers()
-            self.wfile.write(time.asctime())
             self.wfile.write(DATA.encode('GBK'))
+            my_logger.warning('from ip: %s request' % self.client_address[0])
         else:
             # self.send_response(404)
-            print(self.client_address)
-            logging.basicConfig(filename='deny.log', format='%(asctime)s - %(levelname)s - %(message)s')
-            logging.warning('%s is denied' % self.client_address[0])
+            # print(self.client_address)
+            my_logger.warning('%s is denied' % self.client_address[0])
 
-        message = threading.currentThread().getName()
-        print(message)
+            # message = threading.currentThread().getName()
+            # print(message)
 
 
 def get_data_from_main():
     global MAIN_URL, LOGIN_PARAMS, MAIN_SEC, DATA, REPEAT
-    req = requests.get(MAIN_URL, params=LOGIN_PARAMS)
-    DATA = req.text
-    if REPEAT:
-        # print(time.asctime())
-        threading.Timer(MAIN_SEC, get_data_from_main).start()
+    try:
+        req = requests.get(MAIN_URL, params=LOGIN_PARAMS)
+        DATA = req.text
+    except Exception:
+        print('timeout')
+    print(time.asctime())
+    threading.Timer(MAIN_SEC, get_data_from_main).start()
 
 
 # initial server config parameter
 def init_config():
     global HOST, PORT, LOGIN_PARAMS, MAIN_URL, MAIN_SEC, ACL
     config = ConfigParser.ConfigParser()
-    config.read('/home/gino/Workspace/lpfws/config.ini')
+    config.read('config.ini')
 
     try:
         # Server config
@@ -80,46 +83,57 @@ def init_config():
         # ACL config
         ACL = tuple(config.get('ACL', 'WLIST').split(','))
     except Exception as e:
-        print(e.message)
+        my_logger.warning(e.message)
 
 
-class App():
-    def __init__(self):
-        self.stdin_path = '/dev/null'
-        self.stdout_path = '/dev/tty'
-        self.stderr_path = '/dev/tty'
-        self.pidfile_path = '/tmp/testdaemon.pid'
-        self.pidfile_timeout = 5
+class MyThread(threading.Thread):
+    def __init__(self, main_url, login_params, main_sec):
+        threading.Thread.__init__(self)
+        self._main_url = main_url
+        self._login_params = login_params
+        self._main_sec = main_sec
+        self._stop_flag = False
 
     def run(self):
-        # initial server
-        init_config()
-        # This function will start a new thread via Timer module
-        get_data_from_main()
+        global DATA
+        while not self._stop_flag:
+            try:
+                req = requests.get(self._main_url, params=self._login_params, timeout=0.3)
+                DATA = req.text
+            except Exception:
+                print('timeout')
+            print(time.asctime())
+            time.sleep(self._main_sec)
 
-        # Start a http server
-        MyHandler.server_version = 'Light Forwarding Server/1.0'
-        MyHandler.sys_version = ''
-        httpd = ThreadedHTTPServer((HOST, PORT), MyHandler)
-        print time.asctime(), "Server Starts - %s:%s" % (HOST, PORT)
-        try:
-            httpd.serve_forever()
-        except KeyboardInterrupt:
-            pass
-        httpd.server_close()
-        print time.asctime(), "Server Stops - %s:%s" % (HOST, PORT)
+    def stop(self):
+        self._stop_flag = True
 
 
 if __name__ == '__main__':
-    app = App()
-    logger = logging.getLogger("DaemonLog")
-    logger.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    handler = logging.FileHandler("/home/gino/Workspace/lpfws/testdaemon.log")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    # initial server
+    init_config()
+    # This function will start a new thread via Timer module
+    # get_data_from_main()
+    thread = MyThread(MAIN_URL, LOGIN_PARAMS, MAIN_SEC)
+    thread.start()
 
-    daemon_runner = runner.DaemonRunner(app)
-    # This ensures that the logger file handle does not get closed during daemonization
-    daemon_runner.daemon_context.files_preserve = [handler.stream]
-    daemon_runner.do_action()
+    # logging.basicConfig(filename='fws%s.log' % str(datetime.date.today()), format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+    my_logger = logging.getLogger("requests.packages.urllib3")
+    my_logger.setLevel(logging.INFO)
+    handler = logging.handlers.TimedRotatingFileHandler('run.log', when="midnight", backupCount=5)
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    my_logger.addHandler(handler)
+    my_logger.info('initial config success')
+
+    # Start a http server
+    MyHandler.server_version = 'Light Forwarding Server/1.0'
+    MyHandler.sys_version = ''
+    httpd = ThreadedHTTPServer((HOST, PORT), MyHandler)
+    my_logger.info("Server Starts - %s:%s" % (HOST, PORT))
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    thread.stop()
+    my_logger.info("Server Stops - %s:%s" % (HOST, PORT))
